3acebccad69520451a54a4cdf460c174658c2732
Brian Yang <youngzm339@outlook.com>
2023-07-14 18:06:50 +0800

    [Update] Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Deploy starting ..."
+sudo apt install python3 python3 python-is-python3 -y
+pip install -r requirements.txt
+export PYTHONUNBUFFERED=1
+export DJANGO_SETTINGS_MODULE=fault_diagnosis_system.settings
+python ./manage.py makemigrations
+python ./manage.py migrate
+python ./manage.py runserver 0.0.0.0:80000
